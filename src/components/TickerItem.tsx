e6d1a0e5f2e56041689dd034a8e207ff9f342629
v.olyunin <valeriy-olyunin@yandex.ru>
2021-10-26 07:56:33 +0300

    Animation issues

--- src/components/TickerItem.tsx
+++ src/components/TickerItem.tsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {ITickerItem} from "../types/Tickers";
 import {observer} from "mobx-react";
+import {CSSTransition, SwitchTransition} from "react-transition-group";
+import {computed} from "mobx";
 
 interface ITickerItemViewProps {
     handleClick: (ticker: ITickerItem) => React.MouseEvent
@@ -9,13 +11,34 @@ interface ITickerItemViewProps {
 
 @observer
 class TickerItem extends Component<ITickerItemViewProps> {
+
+    @computed
+    get listView() {
+        const {id, ...fields} = this.props.ticker
+        return fields
+    }
+
     render() {
         return (
-            <tr className="cursor-pointer" onClick={() => this.props.handleClick(this.props.ticker)} key={this.props.ticker.id}>
-                <td>{this.props.ticker.name}</td>
-                <td>{this.props.ticker.last}</td>
-                <td>{this.props.ticker.highestBid}</td>
-                <td>{this.props.ticker.percentChange + '%'}</td>
+            <tr
+                className="cursor-pointer"
+                onClick={() => this.props.handleClick(this.props.ticker)}
+                key={this.props.ticker.id}
+            >
+                {Object.entries(this.listView).map(pair => {
+                    const [key, value] = pair
+                    return (
+                        <SwitchTransition key={`${value}_${key}`} mode='out-in'>
+                            <CSSTransition
+                                key={`${key}_${value}`}
+                                timeout={300}
+                                classNames="fade"
+                            >
+                                <td>{value}</td>
+                            </CSSTransition>
+                        </SwitchTransition>
+                    )
+                })}
             </tr>
         );
     }
